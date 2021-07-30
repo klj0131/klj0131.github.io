986d07b89cb9a274191319df70aad4819fedb4be
klj0131 <klj0131@163.com>
2021-07-30 10:38:05 +0800

    Site updated: 2021-07-30 10:38:05

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","f420343c26c72edaf927edee7134d795"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
